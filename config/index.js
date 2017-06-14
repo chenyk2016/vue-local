008e5e303dc46c39989a02253d8a9d3c49dae0f8
chenyk2016 <1416467526@qq.com>
2017-06-14 16:53:30 +0800

    1

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     // 将./改为 /dist/ 路径固定
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
